556aacfb0814e66098e3b22e81493c0a4c424605
kutthareeya <kutthareeya.sue@gmail.com>
2017-05-30 11:22:17 +0700

    intent to register

--- app/src/main/java/kasembhundit/kutthareeya/thiti/manageres/AuthenActivity.java
+++ app/src/main/java/kasembhundit/kutthareeya/thiti/manageres/AuthenActivity.java
@@ -1,13 +1,41 @@
 package kasembhundit.kutthareeya.thiti.manageres;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AuthenActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView registerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authen);
+
+        //Initial View
+
+        initialView();
+
+        //Register Controler
+        registerControler();
+    } //Main method
+
+    private void registerControler() {
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AuthenActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-}
+
+    private void initialView() {
+        registerTextView = (TextView) findViewById(R.id.txtRegister);
+    }
+
+} //Main Class
