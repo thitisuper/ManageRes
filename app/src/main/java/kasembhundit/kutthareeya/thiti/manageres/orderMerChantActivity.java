1ee2f8863a03bbde67f6ae0ce6d5ea3eba6078bd
kutthareeya <kutthareeya.sue@gmail.com>
2017-08-31 22:13:50 +0700

    good

---
+++ app/src/main/java/kasembhundit/kutthareeya/thiti/manageres/orderMerChantActivity.java
@@ -0,0 +1,13 @@
+package kasembhundit.kutthareeya.thiti.manageres;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class orderMerChantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_mer_chant);
+    }
+}
